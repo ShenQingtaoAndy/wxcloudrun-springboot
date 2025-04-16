c4b2a9bc311ede3d565bbce1e45f407f4359a89d
qingtao.shen <qingtao.shen@gehealthcare.com>
2025-04-17 00:46:13 +0800

    update 0416

--- src/main/java/com/tencent/wxcloudrun/controller/TestController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/TestController.java
@@ -4,6 +4,7 @@ package com.tencent.wxcloudrun.controller;
 import com.alibaba.fastjson.JSONObject;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
@@ -22,6 +23,7 @@ import javax.servlet.http.HttpSession;
 import java.io.Serializable;
 import java.util.Map;
 
+@Slf4j
 @Named
 @ViewScoped
 public class TestController implements Serializable {
@@ -41,6 +43,8 @@ public class TestController implements Serializable {
     @Setter
     private UploadedFile originalImageFile;
 
+
+    private String openId;
 
     @Getter
     @Setter
@@ -94,6 +98,12 @@ public class TestController implements Serializable {
 
 
     public String getOpenId() throws WxErrorException {
+
+        if (!Strings.isEmpty(this.openId)) {
+            log.info("has openId:{}", openId);
+            return this.openId;
+        }
+
         // 从请求头或者参数中获取code（微信授权码）
         ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
         HttpServletRequest request  = (HttpServletRequest)externalContext.getRequest();
@@ -102,13 +112,16 @@ public class TestController implements Serializable {
         if(Strings.isEmpty(code)){
             return "none";
         }
+        log.info("code:{}", code);
         WxMpDefaultConfigImpl config = new WxMpDefaultConfigImpl();
         config.setAppId(APP_ID);
         config.setSecret(SECRET);
         WxMpService wxMpService = new WxMpServiceImpl();
         wxMpService.setWxMpConfigStorage(config);
         // 通过code换取用户的OpenId等信息
-        String openId = wxMpService.getOAuth2Service().getAccessToken(code).getOpenId();
+        String newOpenId = wxMpService.getOAuth2Service().getAccessToken(code).getOpenId();
+        log.info("openId:{}", newOpenId);
+        this.openId = newOpenId;
         return openId;
     }
 
